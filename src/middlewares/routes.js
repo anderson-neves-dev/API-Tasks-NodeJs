705bcd4533ef3c16db3663caf780b8411a2214a8
Anderson Neves <andersonneves.dev@gmail.com>
2024-10-03 16:04:16 -0300

    feat: added new DELETE method

--- src/middlewares/routes.js
+++ src/middlewares/routes.js
@@ -37,5 +37,15 @@ export const routes = [
             database.insert('Tasks', task)
             return res.writeHead(201).end()
         }
+    },
+    {
+        method: 'DELETE',
+        path: buildRoutePath('/tasks/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+            database.delete('Tasks', id)
+            return res.writeHead(204).end()
+        }
     }
+
 ]
